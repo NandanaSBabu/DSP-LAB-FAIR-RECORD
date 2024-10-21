0f21dad5759629d79d869cf07fc860eef3a086eb
Karthik <221220@tkmce.ac.in>
2024-10-21 09:30:16 +0000

    dft using equation

--- Experiment-7 DFT and IDFT/dft.m
+++ Experiment-7 DFT and IDFT/dft.m
@@ -5,6 +5,7 @@ x=input("enter sequence:");
 N=input("enter the n point:");
 l=length(x);
 x=[x zeros(1,N-1)];
+X=zeros(N,1);
 for k=0:N-1
     for n=0:N-1
         X(k+1)=X(k+1)+x(n+1)*exp(-1j*2*pi*n*k/N);
