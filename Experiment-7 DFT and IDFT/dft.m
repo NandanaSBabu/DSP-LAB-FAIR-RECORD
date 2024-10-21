944e2bba061f42f424f293b6752d6e939fc16e60
Karthik <221220@tkmce.ac.in>
2024-10-21 09:27:10 +0000

    DFT using equation

---
+++ Experiment-7 DFT and IDFT/dft.m
@@ -0,0 +1,19 @@
+clc;
+clear all;
+close all;
+x=input("enter sequence:");
+N=input("enter the n point:");
+l=length(x);
+x=[x zeros(1,N-1)];
+for k=0:N-1
+    for n=0:N-1
+        X(k+1)=X(k+1)+x(n+1)*exp(-1j*2*pi*n*k/N);
+    end
+end
+disp('X');
+disp(X);
+disp('round(X)');
+disp(round(X));
+%verification
+disp('fft');
+disp(fft(x));
