5d5642fbe1fa88230f43baa11f091b873748d45c
NandanaSBabu <133081714+NandanaSBabu@users.noreply.github.com>
2024-11-07 17:44:54 +0530

    hamming_window.m

--- Experiment-9/lowpass_filter/hamming_window.m
+++ Experiment-9/lowpass_filter/hamming_window.m
@@ -13,12 +13,12 @@ w = 0:0.01:pi;
 h = freqz(hn,1,w);
 subplot(2,1,1);
 plot(w/pi,10*log10(abs(h)));
-title('low pass filter using rectangular window');
+title('low pass filter using hamming window');
 xlabel('Normalized frequency');
 ylabel('Magnitude in dB');
 subplot(2,1,2);
 stem(wr);
-title('Rectangular window Sequence');
+title('Hamming window Sequence');
 xlabel('No. of Samples');
 ylabel('Amplitude');
 
