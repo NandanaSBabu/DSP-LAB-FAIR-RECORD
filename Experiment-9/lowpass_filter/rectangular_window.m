a424a42b7231949ca05ac8613b90288633a63ac4
NandanaSBabu <133081714+NandanaSBabu@users.noreply.github.com>
2024-11-07 17:40:34 +0530

    rectangular_window.m

--- Experiment-9/lowpass_filter/rectangular_window.m
+++ Experiment-9/lowpass_filter/rectangular_window.m
@@ -1,42 +1,24 @@
 clc;
 clear all;
 close all;
-
-% Parameters
-wc = 0.5 * pi;
-N = input("Enter N: ");
-n = 0:1:N-1;
-alpha = (N - 1) / 2;
+wc=0.5*pi;
+N = input('Enter the value of N=');
+alpha = (N-1)/2;
 eps = 0.001;
-
-% Calculate ideal impulse response hd for the low-pass filter
-hd = sin(wc * (n - alpha + eps)) ./ (pi * (n - alpha + eps));
-
-% Apply rectangular window
+n = 0:1:N-1;
+hd = sin(wc*(n-alpha+eps))./(pi*(n-alpha+eps));
 wr = boxcar(N);
-hn = hd .* wr;
-
-% Define frequency range for frequency response
+hn = hd.*wr';
 w = 0:0.01:pi;
-h = freqz(hn(:), 1, w);
-
-% Plot 1: Frequency Response
-subplot(1, 3, 1);
-plot(w / pi, 10 * log10(abs(h)));
-title('Low-pass filter using rectangular window');
-xlabel('Normalized Frequency (\times\pi rad/sample)');
-ylabel('Magnitude (dB)');
-
-% Plot 2: Rectangular Window Sequence
-subplot(1, 3, 2);
-stem(n, wr);
-title('Rectangular window sequence');
-xlabel('Number of samples');
+h = freqz(hn,1,w);
+subplot(2,1,1);
+plot(w/pi,10*log10(abs(h)));
+title('low pass filter using rectangular window');
+xlabel('Normalized frequency');
+ylabel('Magnitude in dB');
+subplot(2,1,2);
+stem(wr);
+title('Rectangular window Sequence');
+xlabel('No. of Samples');
 ylabel('Amplitude');
 
-% Plot 3: Impulse Response of the Filter
-subplot(1, 3, 3);
-stem(n, hn);
-title('Impulse response of the filter');
-xlabel('Number of samples');
-ylabel('Amplitude');
